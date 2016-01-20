98cdc5f61ff1b5912e9d3518f657ee61bcb19b0a
Jay McDaniel <jaymcdan@gmail.com>
2016-01-19 23:59:01 -0500

    blue mtn, wintergreen, weather link

--- scripts_src/build_Resort_Brief_Div.js
+++ scripts_src/build_Resort_Brief_Div.js
@@ -2,35 +2,40 @@ var utils = require('./utils.js');
 /** builds html from _resorts.json and returns it as a .resort_snippet div*/
 
 var buildResortBriefDiv = function (resort_obj) {
-    
+
 
     var name_and_logo = "<a target ='_blank' href ='http://" + resort_obj.contact_info.url + "'><h3><img class ='resort_logo_img' src = 'images/resort_logos/" + resort_obj.images.logo + "'/>" + resort_obj.name + "</h3></a>";
-    
-    var full_address = "<a target ='_blank' href ='" + resort_obj.contact_info.address_url + "'><span class = 'full_address'>" + resort_obj.contact_info.street_address + ", " + resort_obj.contact_info.city + ", " + resort_obj.contact_info.state + " " + resort_obj.contact_info.zip + "</span></a>&nbsp;";
+
+    var trails = "<span class = 'trails report'>" + resort_obj.stats.trails + "&nbsp;trails </span>";
+
+    var lifts = "<span class = 'lifts report'>" + resort_obj.stats.lifts + "&nbsp;lifts </span>";
+
+    var acres = "<span class = 'acres report'>" + resort_obj.stats.skiable_acres + "&nbsp;acres</span>";
+
+    var vert = "<span class = 'vert report'>" + utils.addCommas(resort_obj.stats.vertical_drop_ft) + "'&nbsp;vertical&nbsp;drop</span>";
+
+    var full_address = "<a target ='_blank' href ='" + resort_obj.contact_info.address_url + "'><span class = 'full_address'>" + resort_obj.contact_info.street_address + ", " + resort_obj.contact_info.city + ", " + resort_obj.contact_info.state + " " + resort_obj.contact_info.zip + "</span></a> ";
+
+    var rates = "Rates (midweek / weekend): <span class = 'rates_adult report'>Adult:&nbsp;$" + resort_obj.rates.adults_midweek + "&nbsp;/&nbsp;$" + resort_obj.rates.adults_prime + "&nbsp;</span>\
+                <span class = 'rates_junior report'>Junior:&nbsp;$" + resort_obj.rates.juniors_midweek + "&nbsp;/&nbsp;$" + resort_obj.rates.juniors_prime + "</span> <a class = 'rates_url' target ='_blank' href ='" + resort_obj.rates.rates_url + "'>Tickets</a>";
 
     var phone = "<span class ='resort_phone_number'>" + resort_obj.contact_info.phone.replace(/-/g, "&#8209;") + "</span>";
 
-    var trails = "<span class = 'trails report'>" + resort_obj.stats.trails + "&nbsp;trails </span>";
-    
-    var lifts =  "<span class = 'lifts report'>" + resort_obj.stats.lifts + "&nbsp;lifts </span>";
-    
-    var acres = "<span class = 'acres report'>" + resort_obj.stats.skiable_acres + "&nbsp;acres</span>";
-    
-    var vert = "<span class = 'vert report'>" + utils.addCommas(resort_obj.stats.vertical_drop_ft) + "'&nbsp;vertical&nbsp;drop</span>";
-    
+
     var toggle_widget_link = "<span class = 'toggle_widget_link report' widget_link = " + resort_obj.stats.widget_link + ">more info</span>"
-    
-    var map_thumbnail = "<div class='map_thumbnail'> <a target ='_blank' href='images/resort_maps/"+resort_obj.images.map+"'><img src='images/resort_maps/" + utils.thumbnailName(resort_obj.images.map) + "'/></a></div>";
+
+    var map_thumbnail = "<div class='map_thumbnail'> <a target ='_blank' href='images/resort_maps/" + resort_obj.images.map + "'><img src='images/resort_maps/" + utils.thumbnailName(resort_obj.images.map) + "'/></a></div>";
 
 
-    var snippet = "<div class='resort_snippet' id ='"+ resort_obj.id +"' >" + name_and_logo + "\
-        " + map_thumbnail  + "<div class='resort_snippet_text'>\
+    var snippet = "<div class='resort_snippet' id ='" + resort_obj.id + "' >" + name_and_logo + "\
+        " + map_thumbnail + "<div class='resort_snippet_text'>\
         " + trails + lifts + acres + vert + "<br/>\
-        " + full_address  + phone + "<br/>\
+        " + rates + "<br>\
+        " + full_address + phone + "<br/>\
         " + toggle_widget_link + "</div>\
             <div class='clear_float'></div>\
         </div>";
-    
+
 
 
     return snippet;
--- scripts_src/insert_Current_Weather.js
+++ scripts_src/insert_Current_Weather.js
@@ -2,37 +2,46 @@ var utils = require("./utils.js");
 
 /** builds html from weather json and inserts weather report span */
 
-var insertCurrentWeather = function(json, name){
-    
-    
+var insertCurrentWeather = function (json, resort_obj) {
+
     var weather_report = document.createElement("p");
-        weather_report.className = "weather_report";
-    
+    weather_report.className = "weather_report";
+
     var icon = document.createElement("img");
-        icon.className ="weather_icon";
-        icon.src = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
-    
+    icon.className = "weather_icon";
+    icon.src = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
+
     var description = document.createElement("span");
-        description.className = "weather_description";
-        description.textContent = utils.weatherDescriptor(json.weather[0].description) +" ";
-    
+    description.className = "weather_description";
+    description.textContent = utils.weatherDescriptor(json.weather[0].description) + " ";
+
     var temp = document.createElement("span");
-        temp.className ="weather_temp";
-        temp.textContent = json.main.temp.toFixed(1) + "\u2109"; //symbol for degrees F
-    
+    temp.className = "weather_temp";
+    temp.textContent = json.main.temp.toFixed(1) + "\u2109"; //symbol for degrees F
+
     var wind_speed = document.createElement("span");
-        wind_speed.className = "weather_wind_speed";
-        wind_speed.textContent = "wind:\u00a0" + json.wind.speed.toFixed(1) + "\u00a0mph";
-    
-    
-    
+    wind_speed.className = "weather_wind_speed";
+    wind_speed.textContent = "wind:\u00a0" + json.wind.speed.toFixed(1) + "\u00a0mph";
+
+    var five_day_forcast_span = document.createElement("span");
+    var five_day_forcast_link = document.createElement("a");
+    five_day_forcast_link.className = "five_day_forcast_link";
+    five_day_forcast_link.textContent = "five day forcast";
+    five_day_forcast_link.href = "http://www.weather.com/weather/5day/l/" +resort_obj.contact_info.zip+ ":4:US";
+    five_day_forcast_link.target = "_blank";
+    five_day_forcast_span.appendChild(five_day_forcast_link);
+
+
+
+
     weather_report.appendChild(icon);
     weather_report.appendChild(description);
     weather_report.appendChild(temp);
     weather_report.appendChild(wind_speed);
-    
-    $("#" + name.id + " .resort_snippet_text").prepend(weather_report);
-      
+    weather_report.appendChild(five_day_forcast_span);
+
+    $(weather_report).insertBefore($("#" + resort_obj.id + " .toggle_widget_link"));
+
 }
 
 
