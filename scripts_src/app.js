db38ff4e24d0078e8cf44be9680cce0fd84b2f9a
Jay McDaniel <jaymcdan@gmail.com>
2016-01-20 14:02:30 -0500

    added sort bar

--- scripts_src/app.js
+++ scripts_src/app.js
@@ -1,13 +1,21 @@
 var loadResorts = require("./load_Resorts.js");
 var printResorts = require("./print_Resorts.js");
 var toggleWidgetDisplaySetup = require("./toggle_Widget_Display_setup.js");
+var sortBarSetup = require("./sort_Bar_setup.js");
 
 $(document).ready(function(){
     
+    /** get the resort json from selected area */
     loadResorts("DC", function(json, area){
+        
         printResorts(json, area);
         toggleWidgetDisplaySetup();
+        sortBarSetup(json, area);
 
     });
+    
+    
+    
+    
         
 });
