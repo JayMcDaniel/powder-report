ec02f4b4315e43db5c1a82b662ac0140ee9e03f3
Jay McDaniel <jaymcdan@gmail.com>
2016-01-23 12:45:54 -0500

    now using sass instead of css

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require("gulp"); //build skeleton
 var gutil = require("gulp-util"); //console util
 var source = require("vinyl-source-stream"); // helps with transfering files
@@ -6,33 +8,55 @@ var watchify = require("watchify"); //tool that watches src for changes and auto
 //var reactify = require("reactify"); //tool that changes jsx to js
 var streamify = require('gulp-streamify');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
 
 
-gulp.task("default", function(){
-  var bundler = watchify(browserify({
-    entries: ["./scripts_src/app.js"],
-   // transform: [reactify],
-    extensions: ['.jsx', '.js'],
-    debug: true,
-    cache: {},
-    packageCache: {},
-    fullPaths: false
-  }));
+gulp.task('sass', function () {
 
-  function build(file){
-    if (file) {
-      gutil.log("Recompiling " + file);
-    }
-    return bundler
-      .bundle()
-      .on("error", gutil.log.bind(gutil, "Browserify Error"))
-      .pipe(source("build/main.js"))
-      .pipe(streamify(uglify()))
-      .pipe(gulp.dest("./"));
-  };
-  build();
-  bundler.on("update", build);
+    gulp.src('./styles/sass/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./styles/css'));
+});
+
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./styles/sass/*.scss', ['sass']);
+});
+
+
+
+
+
+
+gulp.task("default", ['sass','sass:watch'], function () {
+    var bundler = watchify(browserify({
+        entries: ["./scripts_src/app.js"],
+        // transform: [reactify],
+        extensions: ['.jsx', '.js'],
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: false
+    }));
+
+    function build(file) {
+        if (file) {
+            gutil.log("Recompiling " + file);
+        }
+
+
+
+
+        return bundler
+            .bundle()
+            .on("error", gutil.log.bind(gutil, "Browserify Error"))
+            .pipe(source("build/main.js"))
+            .pipe(streamify(uglify()))
+            .pipe(gulp.dest("./"));
+    };
+    build();
+    bundler.on("update", build);
 
 
 });
